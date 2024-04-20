cef47fc8a6861331ce47a511e1262c115169b5f5
Yuri Kuzmichev <48134058+WorldforWorld@users.noreply.github.com>
2024-04-20 14:06:32 +0500

    create project

---
+++ assets/js/music-list.js
@@ -0,0 +1,14 @@
+let allMusic = [
+  {
+    name: 'Для женщин',
+    artist: 'Алекс Мэй',
+    img: 'music-1',
+    src: 'music-1',
+  },
+  {
+    name: 'Для мужчин',
+    artist: 'Алекс Мэй',
+    img: 'music-2',
+    src: 'music-2',
+  },
+];
